5a65af1730d504b2fc7a45d65f76bfa6a56e3f97
noorhatami <116758505+noorhatami@users.noreply.github.com>
2023-06-18 00:05:46 -0400

    updated db URI to work with MongoDB Atlast

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,4 @@
 module.exports={
-    "URI": "mongodb://localhost/business_contacts"
+    //"URI": "mongodb://localhost/business_contacts",
+    "URI": "mongodb+srv://noorhatami:hatanoor234!!@mongodbserver.l8uetsc.mongodb.net/business-contacts?retryWrites=true&w=majority"
 }
